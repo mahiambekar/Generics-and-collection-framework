101b7acb63916ec29aaef3fc42f897caa7d63df7
mahiambekar <66378229+mahiambekar@users.noreply.github.com>
2020-07-07 20:00:16 +0530

    Add files via upload

---
+++ ListInterface_indexOf.java
@@ -0,0 +1,23 @@
+/******************************************************************************
+
+                            Online Java Compiler.
+                Code, Compile, Run and Debug java program online.
+Write your code in this editor and press "Run" button to execute it.
+
+*******************************************************************************/
+import java.util.*;
+public class Main
+{
+	public static void main(String[] args) {
+		ArrayList<String> ob=new ArrayList<>();
+		System.out.println(ob.size());
+		ob.add("A");
+		ob.add("B");
+		ob.add("C");
+		ob.add("D");
+	    ob.add(1,"E");
+	    System.out.println(ob.indexOf("C"));
+	    System.out.println(ob.lastIndexOf("A"));
+	    
+	}
+}
