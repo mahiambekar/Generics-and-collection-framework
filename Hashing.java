072192ae1127e96af8497454ff6aa6a6d24eda0e
mahiambekar <66378229+mahiambekar@users.noreply.github.com>
2020-07-08 17:12:57 +0530

    Add files via upload

---
+++ Hashing.java
@@ -0,0 +1,19 @@
+/******************************************************************************
+
+                            Online Java Compiler.
+                Code, Compile, Run and Debug java program online.
+Write your code in this editor and press "Run" button to execute it.
+
+*******************************************************************************/
+import java.util.*;
+public class Main
+{
+	public static void main(String[] args) {
+		HashSet<Integer> ob=new HashSet<>();
+		ob.add(10);
+		ob.add(20);
+		ob.add(30);
+		ob.add(40);
+		System.out.println(ob);
+	}
+}
