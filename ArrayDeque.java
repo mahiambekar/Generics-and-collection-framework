24b78c2936b544fc58a2abed4e12bb66da3e5600
mahiambekar <66378229+mahiambekar@users.noreply.github.com>
2020-07-09 17:37:50 +0530

    Add files via upload

---
+++ ArrayDeque.java
@@ -0,0 +1,18 @@
+/******************************************************************************
+
+                            Online Java Compiler.
+                Code, Compile, Run and Debug java program online.
+Write your code in this editor and press "Run" button to execute it.
+
+*******************************************************************************/
+import java.util.*;
+public class Main
+{
+	public static void main(String[] args) {
+		ArrayDeque<Integer> ob= new ArrayDeque<>();
+		ob.push(10);
+		ob.push(20);
+		ob.push(30);
+		System.out.println(ob.pop());
+	}
+}
